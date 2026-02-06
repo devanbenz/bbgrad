32c5017b9f840cf18a9e1f04f1c64b84881c2e1f
devanbenz <devandbenz@gmail.com>
2026-02-06 10:49:00 -0600

    feat: Thinking more about how I can infer the shape in relation to how the data is structured when it is passed in to my Tensor I have a few things to think about, 1. Creation of a nested vec type that can infer shape 2. Using the builder pattern for my Tensor

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
+use std::any::TypeId;
 use std::fmt::{Debug, Display};
-use std::mem::offset_of;
 use std::rc::Rc;
 
 use ndarray::{ArrayBase, IxDyn, OwnedRepr};
@@ -12,65 +12,105 @@ enum TensorDevice {
     Gpu,
 }
 
+#[derive(Debug)]
+enum TensorOp {
+    Add,
+    Sub,
+}
+
 #[derive(Debug, Clone)]
 enum TensorDtype {
-    Float64(f64),
-    Float32(f32),
-    Int64(i64),
-    Int32(i32),
+    Float64,
+    Float32,
+    Int64,
+    Int32,
 }
 
 #[derive(Debug)]
-enum TensorInner {
-    List(Vec<TensorDtype>),
-    Scalar(TensorDtype),
-    Tensor(Tensor),
-    NdArray(ArrayBase<OwnedRepr<TensorDtype>, IxDyn, TensorDtype>),
+enum TensorInner<T>
+where
+    T: Clone + Debug,
+{
+    List(Vec<T>),
+    Scalar(T),
+    Tensor(Tensor<T>),
+    NdArray(ArrayBase<OwnedRepr<T>, IxDyn, T>),
 }
 
-struct TensorData {
-    inner: TensorInner,
+struct TensorData<T>
+where
+    T: Clone + Debug,
+{
+    inner: TensorInner<T>,
     dtype: TensorDtype,
 }
 
-impl TensorData {
-    pub fn new(dtype: TensorDtype, inner: TensorInner) -> Self {
+impl<T> TensorData<T>
+where
+    T: Clone + Debug + 'static,
+{
+    pub fn new(dtype: TensorDtype, inner: TensorInner<T>) -> Self {
+        match dtype {
+            TensorDtype::Float64 => {
+                assert_eq!(TypeId::of::<f64>(), TypeId::of::<T>())
+            }
+            TensorDtype::Float32 => {
+                assert_eq!(TypeId::of::<f32>(), TypeId::of::<T>())
+            }
+            TensorDtype::Int64 => {
+                assert_eq!(TypeId::of::<i64>(), TypeId::of::<T>())
+            }
+            TensorDtype::Int32 => {
+                assert_eq!(TypeId::of::<i32>(), TypeId::of::<T>())
+            }
+        }
         Self { inner, dtype }
     }
 }
 
 #[derive(Debug)]
-struct Tensor {
-    data: ArrayBase<OwnedRepr<TensorDtype>, IxDyn, TensorDtype>,
+struct Tensor<T>
+where
+    T: Clone + Debug,
+{
+    data: ArrayBase<OwnedRepr<T>, IxDyn, T>,
     grad: Option<i32>,
     requires_grad: bool,
-    input: Option<Rc<Tensor>>,
+    input: Option<Rc<Tensor<T>>>,
     device: TensorDevice,
     dtype: TensorDtype,
+    op: Option<TensorOp>,
 }
 
-impl Tensor {
-    pub fn new(shape: &[usize], data: TensorData) -> Self {
+impl<T> Tensor<T>
+where
+    T: Clone + Debug,
+{
+    // TODO: Right now I require a shape for tensors, it would be nice to implement
+    // something like a Vec<NestedList<T>> for this and infer the shape similar to
+    // what numpy can do. There is overhead to this though, I need to see how python handles
+    // this, my assumption is that numpy has some specialized C code for this.
+    pub fn new(shape: &[usize], data: TensorData<T>) -> Self {
         let arr: ArrayBase<_, IxDyn, _> = match data.inner {
             TensorInner::List(items) => ArrayBase::from_shape_vec(shape, items).unwrap(),
             TensorInner::Scalar(item) => ArrayBase::from_shape_vec(shape, vec![item]).unwrap(),
             TensorInner::Tensor(tensor) => tensor.data,
             TensorInner::NdArray(array_base) => {
                 if array_base.shape() != shape {
-                    let a = array_base.to_shape(shape).unwrap().to_owned();
-                    ArrayBase::from(a)
+                    array_base.to_shape(shape).unwrap().to_owned()
                 } else {
-                    ArrayBase::from(array_base)
+                    array_base
                 }
             }
         };
         Self {
             data: arr,
             grad: None,
-            requires_grad: false,
+            requires_grad: true,
             input: None,
             device: TensorDevice::Cpu,
             dtype: data.dtype,
+            op: None,
         }
     }
 
@@ -79,15 +119,10 @@ impl Tensor {
     }
 }
 
-impl std::ops::Add for Tensor {
-    type Output = Tensor;
-
-    fn add(self, rhs: Self) -> Self::Output {
-        todo!()
-    }
-}
-
-impl Display for Tensor {
+impl<T> Display for Tensor<T>
+where
+    T: Clone + Debug,
+{
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{:?}", self.data)
     }
@@ -98,16 +133,19 @@ fn main() {}
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::any::{Any, TypeId};
+
+    #[test]
+    #[should_panic]
+    fn tensor_data_test() {
+        let _ = TensorData::new(TensorDtype::Float64, TensorInner::Scalar(15f64));
+        let _ = TensorData::new(
+            TensorDtype::Float64,
+            TensorInner::Scalar("Should panic!".to_string()),
+        );
+    }
 
     #[test]
     fn tensor_test() {
-        let tensor = Tensor::new(
-            &[2, 2],
-            TensorData::new(TensorDtype::Float64(_), vec![1., 2., 3., 4.]),
-        );
-        println!("{:?}", tensor);
-        let tensor2 = Tensor::new(&[2, 2], TensorData::Tensor(tensor));
-        println!("{:?}", tensor2.data.type_id());
+        let _data = TensorData::new(TensorDtype::Float64, TensorInner::Scalar(15f64));
     }
 }
